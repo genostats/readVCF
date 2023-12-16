c889800c387dac0dd46b8c7a69e6bc28e4e68f0c
HervePerdry <herve.perdry@gmail.com>
2023-12-16 08:30:45 +0100

    deleted test pointer

--- tests/test_pointer.r
+++
@@ -1,8 +0,0 @@
-filepath <- system.file("extdata", "LCT.vcf.gz", package = "readVCF")
-test_htsVCF(filepath, c("2:136402646-136402781", "2:136408821-136408831"))
-test_pointer(filepath, c("2:136402646-136402781", "2:136408821-136408831"))
-
-
-filepath2 <- system.file("extdata", "index.vcf.gz", package = "readVCF")
-test_htsVCF("inst/extdata/index.vcf.gz", "10:-")
-
